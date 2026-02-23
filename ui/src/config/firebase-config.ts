ebd8aefdff0ea4e6767db05cf2f629e84e8e5839
krisha bhingaradiya <krishabhingaradiya1234@gmail.com>
2026-02-23 12:55:11 +0530

    `Updated Prisma and Firebase configurations`

--- ui/src/config/firebase-config.ts
+++ ui/src/config/firebase-config.ts
@@ -7,13 +7,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyBATB8jiXaS6jJ1-Fo83RSj3rYLTfbAPGw',
-  authDomain: 'dairyflow-c0105.firebaseapp.com',
-  projectId: 'dairyflow-c0105',
-  storageBucket: 'dairyflow-c0105.firebasestorage.app',
-  messagingSenderId: '171585210897',
-  appId: '1:171585210897:web:189c5aa8b5ba797240ffb0',
-  measurementId: 'G-TM5TC4K8R7',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
